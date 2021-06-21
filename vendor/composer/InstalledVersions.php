d510052fc12c04800536c55647a8dbcb54c82d79
rusdeman <rusdeman@gmail.com>
2021-06-21 15:44:53 +0300

    delete unused reqs

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -25,24 +25,24 @@ class InstalledVersions
     private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => 'b2b5802a937e53c6bcd79a7e779738e434e3ae02',
     'name' => 'das/dungeon',
   ),
   'versions' => 
   array (
     'das/dungeon' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => 'b2b5802a937e53c6bcd79a7e779738e434e3ae02',
     ),
   ),
 );
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,24 +1,24 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => 'b2b5802a937e53c6bcd79a7e779738e434e3ae02',
     'name' => 'das/dungeon',
   ),
   'versions' => 
   array (
     'das/dungeon' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => 'b2b5802a937e53c6bcd79a7e779738e434e3ae02',
     ),
   ),
 );
